94672e7c0a3ac9d3b643e005307da6fdba1bf92f
pinto0309 <rmsdh122@yahoo.co.jp>
2025-07-25 00:10:07 +0900

    weight balance

--- src/human_edge_detection/experiments/config_manager.py
+++ src/human_edge_detection/experiments/config_manager.py
@@ -73,7 +73,7 @@ class TrainingConfig:
 
     # Validation
     validate_every: int = 1
-    save_every: int = 1
+    save_every: int = 10
     early_stopping_patience: int = 10
 
     # Loss weights
